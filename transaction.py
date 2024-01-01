03f059ea943aabc629ab88ec10fcd3825c7db2f5
oliabhi <oliabhi26@gmail.com>
2024-01-01 23:24:49 +0530

    checkpoint - port confirmed_tran to gcp

--- transaction.py
+++ transaction.py
@@ -1,43 +1,58 @@
-'''
-Transactions.py : Routes details about confirmed orders back! 
-Info : customer_id,customer_email,total_cost
-'''
-
-
 import json
-
-from kafka import KafkaConsumer
-from kafka import KafkaProducer
-
+from kafka import KafkaConsumer, KafkaProducer
+from confluent_kafka import avro, avro_serializer
+from confluent_kafka.avro.serializer import SerializerError
+from google.cloud import bigquery
+from google.cloud.bigquery import WriteDisposition
 
 ORDER_KAFKA_TOPIC = "order_details"
 ORDER_CONFIRMED_KAFKA_TOPIC = "order_confirmed"
+'''
+init GCP PROJECT_ID TO PORT CONFIRMED ORDERS TO BIGQUERY TABLES.
+'''
+BIGQUERY_PROJECT_ID = "test"
+BIGQUERY_DATASET_ID = "orders"
+BIGQUERY_TABLE_ID = "CONFIRMED_ORDERS"
 
 consumer = KafkaConsumer(
-    ORDER_KAFKA_TOPIC, 
+    ORDER_KAFKA_TOPIC,
     bootstrap_servers="localhost:29092"
 )
 producer = KafkaProducer(bootstrap_servers="")
 
+# Configure the BigQuery client
+client = bigquery.Client(project=BIGQUERY_PROJECT_ID)
+dataset_ref = client.dataset(BIGQUERY_DATASET_ID)
+table_ref = dataset_ref.table(BIGQUERY_TABLE_ID)
+table = client.get_table(table_ref)
 
 print("Gonna start listening")
+
 while True:
     for message in consumer:
         print("Ongoing transaction..")
         consumed_message = json.loads(message.value.decode())
         print(consumed_message)
+
         user_id = consumed_message["user_id"]
         total_cost = consumed_message["total_cost"]
-        data = {
-            "customer_id": user_id,
-            "customer_email": f"{user_id}@gmail.com",
-            "total_cost": total_cost
-        }
-        print("Successful transaction..")
-        producer.send(ORDER_CONFIRMED_KAFKA_TOPIC, json.dumps(data).encode("utf-8"))
 
+        # Prepare data for BigQuery
+        data = [
+            {
+                "customer_id": user_id,
+                "customer_email": f"{user_id}@gmail.com",
+                "total_cost": total_cost
+            }
+        ]
 
-'''
-Will be extending routing the transaction routed as confirmed orders to GCP bigquery
-using kafka bigquery connector
-'''
+        # Write data to BigQuery
+        errors = client.insert_rows(table, data)
+
+        if errors:
+            print(f"Error inserting rows to BigQuery: {errors}")
+        else:
+            print("Successful transaction..")
+
+        # Forward the message to another Kafka topic if needed
+        producer.send(ORDER_CONFIRMED_KAFKA_TOPIC, json.dumps(consumed_message).encode("utf-8"))
